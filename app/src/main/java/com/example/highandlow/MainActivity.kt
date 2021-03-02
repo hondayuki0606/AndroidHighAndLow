0d182417aa511cdfd56d363b5ec39ab690b50780
honda <hondayuki0606@yahoo.co.jp>
2021-03-02 22:29:02 +0900

    refs #2021/02/27 hit and lose add プログラムを作成する P121

--- app/src/main/java/com/example/highandlow/MainActivity.kt
+++ app/src/main/java/com/example/highandlow/MainActivity.kt
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private var tag = "high and low"
+    private var yourCard = 0
+    private var droidCard = 0
+    private var hitCount = 0
+    private var loseCount = 0
+    private var gameStart = false
+    private var anserwed = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
